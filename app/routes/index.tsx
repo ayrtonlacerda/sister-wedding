6353e206057fca57b5a70b85ed76dc0d8b22ddde
ayrtonlacerda <ayrtonrodrigues.lacerda@gmail.com>
2022-07-03 19:08:00 -0300

    intall prettier and tailwind

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
       <h1>Day e Digão em Barra Grande</h1>
     </div>
-  );
+  )
 }
